a4411c0f386e14ee7fea036cb17cfc314431ae94
Philipp <philbo96@gmail.com>
2019-04-16 09:03:26 +0200

    Calc: a good start!

--- template/calculator/scripts/calculator.js
+++ template/calculator/scripts/calculator.js
@@ -11,9 +11,35 @@ class Calculator {
 /**
  * UI
  */
+
+function numberPressed (event){
+    console.log("number pressed: " + event.target.value);
+}
+
+function operatorPressed(event){
+    console.log("operator pressed: " + event.target.value);
+}
+
+function commandPressed(event){
+    console.log("command pressed" + event.target.id);
+}
+
 window.addEventListener('DOMContentLoaded', function() {
     const calculator = new Calculator();
+    const numbers = Array.from(document.getElementsByClassName("number"));
+    for(const number of numbers){
+        number.addEventListener("click", numberPressed);
+    }
 
+    const operators = Array.from(document.getElementsByClassName("operator"));
+    for(let element of operators){
+        element.addEventListener("click", operatorPressed);
+    }
+
+    const commands = Array.from(document.getElementsByClassName("command"));
+    for(let element of commands){
+        element.addEventListener("click", commandPressed);
+    }
 });
 
 
