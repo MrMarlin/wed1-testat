472a963fefacad9e0666fc5d8276240a8ce1fc1c
Philipp <philbo96@gmail.com>
2019-04-17 14:06:49 +0200

    Calc: Calc funzt

--- template/calculator/scripts/calculator.js
+++ template/calculator/scripts/calculator.js
@@ -3,30 +3,114 @@
  */
 class Calculator {
     constructor() {
+        this.setupOperatorDict();
+        this.reset();
+    }
 
+    setupOperatorDict(){
+        this.operatorDict = {};
+        this.operatorDict["+"] = function(left, right){
+            return parseInt(left)+ parseInt(right);
+        }
+        this.operatorDict["-"] = function(left, right){
+            return left - right;
+        }
+        this.operatorDict["*"] = function(left, right){
+            return left * right;
+        }
+        this.operatorDict["/"] = function(left, right){
+            if(parseInt(right) == 0){
+                return "Invalid Operation!"
+            }
+            return left / right;
+        }
+    }
+
+    softReset(){
+        this.left = "";
+        this.right = "";
+        this.operator = null;
+    }
+
+    reset(){
+        this.lastResult = "0";
+        this.output = null;
+        this.input = null;
+        this.softReset();
+    }
+
+    setOperator(operator){
+        if(this.left == ""){
+            this.left = this.lastResult;
+        }
+        if(this.operator == null){
+            this.input = "";
+        }
+        this.operator = operator;
+        this.output = this.left + operator;
+    }
+
+    setNumber(number){
+        if(this.operator == null){
+            this.left = this.left + number;
+            this.input = this.left;
+        } else {
+            this.right += number;
+            this.input = this.right;
+        }
+    }
+
+    calculate(){
+        if(this.left == "" || this.operator == null || this.right == ""){
+            return;
+        }
+        this.input = this.operatorDict[this.operator](this.left, this.right);
+        if (this.input == "Invalid Operation!"){
+            this.lastResult = 0;
+        } else{
+            this.lastResult = this.input;
+        }
+        this.output = this.left + this.operator + this.right;
+        this.softReset();
     }
 }
-
 
 /**
  * UI
  */
 
+const calculator = new Calculator();
+let input;
+let output;
+
+function refresh(){
+    input.innerHTML = calculator.input;
+    output.innerHTML = calculator.output;
+}
+
 function numberPressed (event){
-    console.log("number pressed: " + event.target.value);
+    calculator.setNumber(event.target.value);
+    refresh();
 }
 
 function operatorPressed(event){
-    console.log("operator pressed: " + event.target.value);
+    calculator.setOperator(event.target.value);
+    refresh();
 }
 
 function commandPressed(event){
-    console.log("command pressed" + event.target.id);
+    if(event.target.id == "key-=") {
+        calculator.calculate();
+    }else{
+        calculator.reset();
+    }
+    refresh();
 }
 
 window.addEventListener('DOMContentLoaded', function() {
-    const calculator = new Calculator();
     const numbers = Array.from(document.getElementsByClassName("number"));
+    input = document.getElementById("input");
+    output = document.getElementById("output");
     for(const number of numbers){
         number.addEventListener("click", numberPressed);
     }
@@ -41,7 +125,6 @@ window.addEventListener('DOMContentLoaded', function() {
         element.addEventListener("click", commandPressed);
     }
 });
-
 
 /**
  * Tests Scenarios
